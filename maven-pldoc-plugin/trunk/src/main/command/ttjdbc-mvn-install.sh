1214e1733cdc3f755dca6a6faed1545505c5c6e3
sturton <sturton@9164c0f4-9545-4e91-8a7c-0c4768dc9b1a>
2018-01-14 00:37:59 +0000

    maven-pldoc-plugin: password test resources

--- maven-pldoc-plugin/trunk/src/main/command/ttjdbc-mvn-install.sh
+++ maven-pldoc-plugin/trunk/src/main/command/ttjdbc-mvn-install.sh
@@ -9,7 +9,7 @@ set -xv
 #mvn install:install-file -Dfile=pldoc/target/.jar -Dpackaging=jar -DgroupId=net.sourceforge.pldoc -DartifactId=maven-pldoc-plugin -Dversion=2.15-SNAPSHOT -Dpackage=jar -DgeneratePom=true 
 
 #Install the TimesTen JDBC JAR file in the local repository, so that the Oracle JDBC jar file(s) are available as a dependency  
-#mvn install:install-file -Dfile=${ORACLE_HOME}/orai18n.jar -Dpackaging=jar -DgroupId=com.oracle -DartifactId=ojdbc14 -Dversion=10.2.0.3 -Dpackage=jar 
+#mvn install:install-file -Dfile=${ORACLE_HOME}/orai18n.jar -Dpackaging=jar -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=10.2.0.3 -Dpackage=jar 
 
 mvn install:install-file -Dfile=${TT_HOME}/lib/ttjdbc6.jar -Dpackaging=jar -DgroupId=com.oracle -DartifactId=ttjdbc6 -Dversion=11.2.2 -Dpackage=jar -DgeneratePom=true 
 
